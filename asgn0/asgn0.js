61f3ef8114766ec3132cd70fe36ab07d2bba8b1b
Kevin Hickey <khickey754@gmail.com>
2026-01-11 21:26:15 -0800

    Step 2

--- asgn0/asgn0.js
+++ asgn0/asgn0.js
@@ -10,7 +10,28 @@ function main() {
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
 
-  // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+  // black background
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, 400, 400)
+
+  // create vector
+  var v1 = new Vector3([2.25, 2.25, 0]);
+
+  // draw it
+  drawVector(ctx, v1, "red");
+}
+
+function drawVector(ctx, v, color) {
+  var scale = 20;
+  var centerX = 200;
+  var centerY = 200;
+
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(centerX,centerY);
+  ctx.lineTo(
+    centerX + v.elements[0] * scale,
+    centerY - v.elements[1] * scale
+  );
+  ctx.stroke();
 }
